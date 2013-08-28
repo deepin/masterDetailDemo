70dc9081c5e4be0d9d2896948e7f0f13fbdc12fb
wansong <swan1st@gmail.com>
2013-08-28 10:13:08 +0800

    add a customed view controller to ctrl the image

---
+++ masterDetailDemo/showimage/iSmartVShowImageViewController.h
@@ -0,0 +1,13 @@
+//
+//  iSmartVShowImageViewController.h
+//  masterDetailDemo
+//
+//  Created by LV on 13-8-28.
+//  Copyright (c) 2013年 LargeV. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface iSmartVShowImageViewController : UIViewController
+@property (strong, nonatomic) UIImageView *theimageview;
+@end
---
+++ masterDetailDemo/showimage/iSmartVShowImageViewController.m
@@ -0,0 +1,39 @@
+//
+//  iSmartVShowImageViewController.m
+//  masterDetailDemo
+//
+//  Created by LV on 13-8-28.
+//  Copyright (c) 2013年 LargeV. All rights reserved.
+//
+
+#import "iSmartVShowImageViewController.h"
+
+@interface iSmartVShowImageViewController ()
+
+@end
+
+@implementation iSmartVShowImageViewController
+@synthesize theimageview;
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
